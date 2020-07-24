cb3d382d96eeabc4ccf2839c3793e945328fb195
Olivia Rodrigues <olivia.rt@gmail.com>
2020-07-23 22:41:09 -0300

    Add header css

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,14 @@ import React from "react";
 
 function Header({ user }) {
   return (
-    <header>
-      {user.displayName}
-      <img className="user-avatar" alt="user avatar" src={user.photoURL} />
+    <header id="userLogin">
+      <h1>Monitor</h1>
+
+      <div>
+        <p>{user.displayName}</p>
+        <img alt="user avatar" src={user.photoURL} />
+      </div>
+
     </header>
   );
 }
